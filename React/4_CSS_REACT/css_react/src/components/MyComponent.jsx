b9f14bd00c0bcf87499a791c3f67c9d3fcb1785d
Jeferson Martins <jeferson.dev@proton.me>
2026-06-17 02:06:15 -0300

    CSS in react

---
+++ React/4_CSS_REACT/css_react/src/components/MyComponent.jsx
@@ -0,0 +1,12 @@
+import "./MyComponent.css";
+
+const MyComponent = () => {
+  return (
+    <div>
+       <h1> CSS de componente</h1>
+    <p>Este parágrafo vai ser um estilo</p>
+    </div>
+  );
+};
+
+export default MyComponent;
